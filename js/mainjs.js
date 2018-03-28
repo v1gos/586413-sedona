a0a7de889f18361998a7adcf0bf74856784ba3bb
v1gos <36693749+v1gos@users.noreply.github.com>
2018-03-28 13:19:04 +0300

    js в отдельный файл + фиксы

--- js/mainjs.js
+++ js/mainjs.js
@@ -1,7 +1,6 @@
-    <script>
-        var jspurchbutton = document.querySelector(".purchise-info-button");
-    var regpopup = document.querySelector(".registration-purchise-container");
-    jspurchbutton.addEventListener("click", function (evt) {
-        evt.preventDefault();
-        regpopup.classList.toggle("registration-purchise-container-show");
-    }); </script>
+var jspurchbutton = document.querySelector(".purchise-info-button");
+var regpopup = document.querySelector(".registration-purchise-container");
+jspurchbutton.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    regpopup.classList.toggle("registration-purchise-container-show");
+    });
