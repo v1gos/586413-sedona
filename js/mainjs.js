b00b1c9816e5405b9ccdf8a50ff9afe0c4e576ec
v1gos <36693749+v1gos@users.noreply.github.com>
2018-03-31 21:01:52 +0300

    minimise/beutify

--- js/mainjs.js
+++ js/mainjs.js
@@ -1,6 +1,6 @@
 var jspurchbutton = document.querySelector(".purchise-info-button");
 var regpopup = document.querySelector(".registration-purchise-container");
-jspurchbutton.addEventListener("click", function (evt) {
+jspurchbutton.addEventListener("click", function(evt) {
     evt.preventDefault();
     regpopup.classList.toggle("registration-purchise-container-show");
-    });
+});
