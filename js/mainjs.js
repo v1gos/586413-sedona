61007fe7039255f6d990b6ef507df0a540a7d5bd
v1gos <36693749+v1gos@users.noreply.github.com>
2018-04-02 18:58:20 +0300

    мин/бьють

--- js/mainjs.js
+++ js/mainjs.js
@@ -3,6 +3,6 @@ var regpopup = document.querySelector(".registration-purchise-container");
 regpopup.classList.add("registration-purchise-container-hide");
 jspurchbutton.addEventListener("click", function(evt) {
     evt.preventDefault();
-	regpopup.classList.toggle("registration-purchise-container-hide");
-	regpopup.classList.toggle("registration-purchise-container-animation");
+    regpopup.classList.toggle("registration-purchise-container-hide");
+    regpopup.classList.toggle("registration-purchise-container-animation");
 });
