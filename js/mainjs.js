0e8527b4c2a23f8930b138324d7dd7cbacd52c2c
v1gos <36693749+v1gos@users.noreply.github.com>
2018-04-02 17:49:37 +0300

    баг с чекбоксами в ие

--- js/mainjs.js
+++ js/mainjs.js
@@ -1,6 +1,8 @@
 var jspurchbutton = document.querySelector(".purchise-info-button");
 var regpopup = document.querySelector(".registration-purchise-container");
+regpopup.classList.add("registration-purchise-container-hide");
 jspurchbutton.addEventListener("click", function(evt) {
     evt.preventDefault();
-    regpopup.classList.toggle("registration-purchise-container-show");
+	regpopup.classList.toggle("registration-purchise-container-hide");
+	regpopup.classList.toggle("registration-purchise-container-animation");
 });
