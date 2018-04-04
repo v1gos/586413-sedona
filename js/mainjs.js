7dd04798e5e424ee30819a581462e490ef0bf16d
v1gos <36693749+v1gos@users.noreply.github.com>
2018-04-04 19:54:08 +0300

    большинство фиксов

--- js/mainjs.js
+++ js/mainjs.js
@@ -1,8 +1,20 @@
-var jspurchbutton = document.querySelector(".purchise-info-button");
-var regpopup = document.querySelector(".registration-purchise-container");
-regpopup.classList.add("registration-purchise-container-hide");
-jspurchbutton.addEventListener("click", function(evt) {
-    evt.preventDefault();
-    regpopup.classList.toggle("registration-purchise-container-hide");
-    regpopup.classList.toggle("registration-purchise-container-animation");
+var jsPurchButton = document.querySelector(".purchise-info-button");
+var regPopup = document.querySelector(".registration-purchise-container");
+var startDate = regPopup.querySelector("[name=start-date]");
+var endDate = regPopup.querySelector("[name=end-date]");
+var adultNumber = regPopup.querySelector("[name=adult-number]");
+var chilrdernNumber = regPopup.querySelector("[name=children-number]");
+regPopup.classList.add("registration-purchise-container-hide");
+jsPurchButton.addEventListener("click", function (evt) {
+	evt.preventDefault();
+	regPopup.classList.toggle("registration-purchise-container-hide");
+	regPopup.classList.toggle("registration-purchise-container-animation");
+});
+regPopup.addEventListener("submit", function (evt) {
+	if (!startDate.value || !endDate.value || !adultNumber.value || !chilrdernNumber.value) {
+		evt.preventDefault();
+		regPopup.classList.remove("modal-error");
+		regPopup.offsetWidth = regPopup.offsetWidth;
+		regPopup.classList.add("modal-error");
+	}
 });
